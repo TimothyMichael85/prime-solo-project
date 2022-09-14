ae7594d8795472114bb13459257b51e5a5dcf165
Tim Anderson <t.anderson0085@gmail.com>
2022-09-14 11:35:29 -0500

    wrote garden saga

---
+++ src/redux/sagas/garden.saga.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+// worker Saga: will be fired on "FETCH_GARDEN actions
+function* fetchGarden() {
+
+
+  try {
+    const config = {
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true,
+    };
+
+//     // the config includes credentials which
+//     // allow the server session to recognize the user
+//     // If a user is logged in, this will return their information
+//     // from the server session (req.user)
+    const response = yield axios.get('/api/garden', config);
+
+//     // now that the session has given us a user object
+//     // with an id and username set the client-side user object to let
+//     // the client-side code know the user is logged in
+    yield put({ type: 'SET_GARDEN', payload: response.data });
+  } catch (error) {
+    console.log('User get request failed', error);
+  }
+}
+
+function* deleteGarden(action) {
+  try {
+    const config = {
+      headers: { 'Content-Type': 'application/json'},
+      withCredentials: true,
+    };
+
+    const response = yield axios.delete(`/api/garden/${action.payload}`, config);
+
+    yield put({type: 'FETCH_GARDEN' });
+  } catch (error) {
+    console.error('deleteGarden failed', error);
+  }
+} // end DELETEGARDEN
+
+function* gardenSaga() {
+  yield takeLatest('FETCH_GARDEN', fetchGarden);
+  yield takeLatest('DELETE_GARDEN', deleteGarden);
+}
+
+export default gardenSaga;

936ce6251da37bfffaa3f9afbc8a5970387ae0ed
Tim Anderson <t.anderson0085@gmail.com>
2022-09-16 15:14:36 -0500

    built add garden form

--- src/components/GardenForm/GardenForm.jsx
+++ src/components/GardenForm/GardenForm.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch} from 'react-redux';
+
 
 
 
 function GardenForm() {
+
+  const dispatch = useDispatch();
+  const [newGarden, setNewGarden] = useState({garden_name: ''});
+
+  const handleNewGarden = (event) => {
+    setNewGarden({...newGarden, garden_name: event.target.value})
+  };
+  
+  const handleSubmit = () => {
+    dispatch({
+        type: 'ADD_GARDEN',
+        payload: newGarden
+    })
+};
+  console.log(newGarden);
   return (
-    <div className="container">
-      <p>Garden Form</p>
-    </div>
+    <>
+        <h3>Add New Garden</h3>
+
+        <form onSubmit={handleSubmit}>
+          <input
+          required
+          placeholder='Garden Name'
+          onChange={handleNewGarden}
+          ></input>
+          <button type="Submit">Submit</button>
+        </form>
+    </>
   );
 }
 
